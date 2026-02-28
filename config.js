b3b2b20a26b875d30c888ffb7e16ccd06a77c3c5
0pal33 <161562932+0pal33@users.noreply.github.com>
2026-03-01 05:27:11 +0700

    Update config.js global function

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const supabase = window.supabase.createClient(
+window.supabase = window.supabase.createClient(
 "https://ktpvkmmuugjcoscxbfja.supabase.co",
 "sb_publishable_1SJL-5yxWSAC3osTXiEdMw_3Q-060K2"
 );
