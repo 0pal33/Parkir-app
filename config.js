b01901272fa43be1535f89c7335cddc549a3e4f7
0pal33 <161562932+0pal33@users.noreply.github.com>
2026-04-11 01:59:14 +0700

    setup v2

--- config.js
+++ config.js
@@ -1,4 +1,19 @@
-window.supabase = window.supabase.createClient(
-"https://ktpvkmmuugjcoscxbfja.supabase.co",
-"sb_publishable_1SJL-5yxWSAC3osTXiEdMw_3Q-060K2"
-);
+const ENV = window.location.hostname.includes("pages.dev")
+  ? "DEV"
+    : "PROD"
+
+    const CONFIG = {
+      DEV: {
+          SUPABASE_URL: "https://vvfaeybdbgsvhifxfzsg.supabase.co",
+              SUPABASE_KEY: "sb_publishable_rFwZ8m11ImicxfYHCwiGnw_l3qKXdvL"
+      },
+        PROD: {
+            SUPABASE_URL: "https://ktpvkmmuugjcoscxbfja.supabase.co",
+                SUPABASE_KEY: "sb_publishable_1SJL-5yxWSAC3osTXiEdMw_3Q-060K2"
+        }
+    }
+
+    const supabase = window.supabase.createClient(
+      CONFIG[ENV].SUPABASE_URL,
+        CONFIG[ENV].SUPABASE_KEY
+    )
