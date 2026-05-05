1175149aafd06891fba4e046f864c826d770171f
0pal33 <161562932+0pal33@users.noreply.github.com>
2026-05-06 02:00:43 +0700

    update js/breakdown.js second fix katanya

--- js/breakdown.js
+++ js/breakdown.js
@@ -5,52 +5,51 @@ function toggleBreakdown(mode){
   renderBreakdown()
 }
 
-const d = window.dashboardData
-
 function renderBreakdown(){
 
-const box = document.getElementById("breakdownBox")
+  const box = document.getElementById("breakdownBox")
+  const d = window.dashboardData
 
-if(!breakdownMode || !window.dashboardData){
-  box.style.display = "none"
-  return
-}
+  if(!breakdownMode || !d){
+    box.style.display = "none"
+    return
+  }
 
-box.style.display = "block"
+  box.style.display = "block"
 
-if(breakdownMode === "income"){
-  box.innerHTML = `
-    <div class="card">
-      <b>Pendapatan</b><br><br>
+  if(breakdownMode === "income"){
+    box.innerHTML = `
+      <div class="card">
+        <b>Pendapatan</b><br><br>
 
-      Parkir:<br>
-      Checkout: ${dashboardData.checkoutCount} (${rupiah(dashboardData.checkoutTotal)})<br>
-      Bulanan: ${dashboardData.bulananCount} (${rupiah(dashboardData.bulananTotal)})<br><br>
+        Parkir:<br>
+        Checkout: ${d.checkoutCount} (${rupiah(d.checkoutTotal)})<br>
+        Bulanan: ${d.bulananCount} (${rupiah(d.bulananTotal)})<br><br>
 
-      Titip Jajan:<br>
-      Item: ${dashboardData.titipanItems}<br>
-      Total: ${rupiah(dashboardData.titipanIncomeTotal)}<br><br>
+        Titip Jajan:<br>
+        Item: ${d.titipanItems}<br>
+        Total: ${rupiah(d.titipanIncomeTotal)}<br><br>
 
-      Stok:<br>
-      Item: ${dashboardData.stokIncomeItems}<br>
-      Total: ${rupiah(dashboardData.stokIncomeTotal)}
-    </div>
-  `
-}
+        Stok:<br>
+        Item: ${d.stokIncomeItems}<br>
+        Total: ${rupiah(d.stokIncomeTotal)}
+      </div>
+    `
+  }
 
-if(breakdownMode === "expense"){
-  box.innerHTML = `
-    <div class="card">
-      <b>Pengeluaran</b><br><br>
+  if(breakdownMode === "expense"){
+    box.innerHTML = `
+      <div class="card">
+        <b>Pengeluaran</b><br><br>
 
-      Titip Jajan:<br>
-      Item: ${dashboardData.titipanItems}<br>
-      Total: ${rupiah(dashboardData.titipanExpenseTotal)}<br><br>
+        Titip Jajan:<br>
+        Item: ${d.titipanItems}<br>
+        Total: ${rupiah(d.titipanExpenseTotal)}<br><br>
 
-      Stok:<br>
-      Item: ${dashboardData.stokExpenseItems}<br>
-      Total: ${rupiah(dashboardData.stokExpenseTotal)}
-    </div>
-  `
-}
+        Stok:<br>
+        Item: ${d.stokExpenseItems}<br>
+        Total: ${rupiah(d.stokExpenseTotal)}
+      </div>
+    `
+  }
 }
